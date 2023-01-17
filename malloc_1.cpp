d334d1a9cba48b6b14bcad152d68f7be87c87515
edenl <edenlevi36@gmail.com>
2023-01-17 22:58:28 +0200

    removed temp functions in malloc_1

--- malloc_1.cpp
+++ malloc_1.cpp
@@ -9,16 +9,10 @@
 #define ERROR (void*)-1
 #define MAX pow(10, 8)
 
-int sbrk(size_t size);
-
 void *smalloc(size_t size) {
     void* result;
     if (size == 0 || (double) size > MAX) return nullptr;
     if ((result = sbrk(size)) == ERROR) return nullptr;
     return result; // + sizeof(size_t) was here
     // if success, return a pointer to the first allocate byte within the allocated block
-}
-
-int sbrk(size_t size) {
-    return 0;
 }
